fbc869ed8508b556b01dfbf6f9b79f55a2877627
AndroidWork <in.glebov@bk.ru>
2022-07-17 13:59:14 +0700

    HomeWork_7

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,12 +2,15 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import androidx.activity.viewModels
 import androidx.annotation.DrawableRes
-import ru.netology.nmedia.data.impl.PostsAdapter
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.databinding.PostBinding
+import ru.netology.nmedia.util.hideKeyboard
+import ru.netology.nmedia.util.showKeyboard
 import ru.netology.nmedia.viewModel.PostViewModel
 
 
@@ -27,12 +30,44 @@ class MainActivity : AppCompatActivity() {
             published = "13.06.22"
         )
 
-        val adapter = PostsAdapter(viewModel::onLikeClicked, viewModel::onShareClicked)
+        val adapter = PostsAdapter(viewModel)
         binding.postsRecyclerView.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
         }
+        binding.saveButton.setOnClickListener {
+            with(binding.contentEditText) {
+                val content = binding.contentEditText.text.toString()
+                viewModel.onSaveButtonClicked(content)
+            }
+        }
+
+        binding.cancelButton.setOnClickListener {
+            with(binding.contentEditText) {
+                viewModel.onEditCancelClicked()
+                val content = binding.contentEditText.text.toString()
+                viewModel.onSaveButtonClicked(content)
+            }
+        }
+
+        viewModel.currentPost.observe(this) { currentPost ->
+            with(binding.contentEditText) {
+                val content = currentPost?.content
+                setText(content)
+                if (content != null) {
+                    binding.contentGroup.visibility = View.VISIBLE
+                    requestFocus()
+                    showKeyboard()
+                } else {
+                    binding.contentGroup.visibility = View.GONE
+                    clearFocus()
+                    hideKeyboard()
+                }
+            }
+        }
     }
 }
+
+
 
 
